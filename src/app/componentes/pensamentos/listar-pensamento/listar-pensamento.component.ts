adfd2659f7541b0e9d3c69a0186594307a30bdb8
f2597529 <f2597529@gmail.com>
2025-10-08 21:21:35 -0300

    entrega aula 4

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decodarada com @Input()',
+      autoria: 'Filho',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'I love angular3, I love angular3, I love angular3, I love angular3, I love angular3, I love angular3, I love angular3,I love angular3,I love angular3,I love angular3, I love angular3, I love angular3, I love angular3, I love angular3, I love angular3, I love angular3, I love angular3',
+      autoria: 'Mary',
+      modelo: 'modelo2'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
